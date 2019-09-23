c2f65ce95dfab73e22692f295f83dd029bda0775
Seriy1574 <kroe74@gmail.com>
2019-09-23 12:56:46 +0300

    23/09

--- Lesson5_part2.sql
+++ Lesson5_part2.sql
@@ -6,12 +6,30 @@
 
 SELECT AVG((TO_DAYS(NOW()) - TO_DAYS(birthday_at))/365.25) AS avg_age
 FROM users;
++-------------+
+| avg_age     |
++-------------+
+| 29.93165903 |
++-------------+
+
 
 SELECT FLOOR(AVG((TO_DAYS(NOW()) - TO_DAYS(birthday_at))/365.25)) AS avg_age
 FROM users;
 
++---------+
+| avg_age |
++---------+
+|      29 |
++---------+
+
+
 SELECT FLOOR(AVG(TIMESTAMPDIFF(YEAR, birthday_at, NOW()))) AS age
 FROM users;
++------+
+| age  |
++------+
+|   29 |
++------+
 
 
 2. Подсчитайте количество дней рождения, которые приходятся на каждый из дней недели. Следует учесть, что необходимы дни недели текущего года, а не года рождения.
@@ -21,6 +39,18 @@ SELECT
  COUNT(u.name) count_users
 from users u
  GROUP BY DAYNAME(DATE_FORMAT(u.birthday_at, '2019-%m-%d'));
+
++-----------+-------------+
+| day_week  | count_users |
++-----------+-------------+
+| Saturday  |           4 |
+| Tuesday   |           3 |
+| Monday    |           2 |
+| Thursday  |           2 |
+| Wednesday |           1 |
+| Friday    |           1 |
++-----------+-------------+
+
 
 Третье задание не смог сделать((
 
