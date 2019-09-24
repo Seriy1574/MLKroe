068cb115a46ce9b4c11eebc2d6ee5a1a6e233aa0
Seriy1574 <kroe74@gmail.com>
2019-09-24 19:23:13 +0300

    Шестой урок

--- Lesson6.sql
+++ Lesson6.sql
@@ -1,4 +1,32 @@
 ﻿
+Виктор, приветствую.
+
+
+1. Проанализировать запросы, которые выполнялись на занятии, определить возможные корректировки и/или улучшения
+(JOIN пока не применять).
+
+По этой задаче все понятно.
+
+SELECT
+  first_name,
+  last_name,
+  (SELECT filename FROM media WHERE id =
+    (SELECT photo_id FROM profiles WHERE user_id = 1)
+  ),
+  (SELECT hometown FROM profiles WHERE user_id = 1)
+  FROM users
+    WHERE id = 1;
+
+/*тут не ограничили тип медиа*/
+
+
+Везде где where like, заменил бы слова на id.
+
+Да и как писал в чате, появляются проблемы, если id не уникальны.
+
+
+=========================================================================================================================
+
 2. Пусть задан некоторый пользователь.
 Из всех друзей этого пользователя найдите человека, который больше всех общался с нашим
 пользоваетелем.
@@ -97,31 +125,99 @@ from
 | Пользователь Christelle Pfannerstill получил 3 сообщ. от пользователя Winfield Rau                                                                                                  |
 +-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
 
-
+========================================================================================================================
 3. Подсчитать общее количество лайков, которые получили 10 самых молодых пользователей
 
-SELECT * FROM profiles;
-
-SELECT user_id, (TIMESTAMPDIFF(YEAR, birthday  NOW())) AS age
-FROM profiles;
-DESCRIBE profiles;
 
 
-+---------------------+
-| Tables_in_VK        |
-+---------------------+
-| communities         |
-| communities_users   |
-| emoji               |
-| friendship          |
-| friendship_statuses |
-| hometowns           |
-| likes               |
-| media               |
-| media_types         |
-| messages            |
-| profiles            |
-| regions             |
-| target_types        |
-| users               |
-+---------------------+
+
+/*тут обернул SELECT  в еще один SELECT , чтою сработал LIMIT 10*/
+
+SELECT COUNT(id) AS All_likes
+ FROM likes AS l
+ WHERE l.user_id IN
+  SELECT * FROM (SELECT user_id
+    FROM profiles
+    ORDER BY (TO_DAYS(NOW()) - TO_DAYS(birthday))/365.25
+    limit 10)t2);
+
++-----------+
+| All_likes |
++-----------+
+|       354 |
++-----------+
+
+========================================================================================================================
+
+4. Определить кто больше поставил лайков (всего) - мужчины или женщины?
+
+
+SELECT t.SEX, COUNT(user_id) AS all_likes
+from(
+    (SELECT l.user_id, 'm' AS SEX
+        FROM likes AS l
+        WHERE l.user_id IN
+             (SELECT user_id
+                 FROM profiles AS p
+                 WHERE p.sex like 'm'))
+     UNION ALL
+    (SELECT l.user_id, 'f' AS SEX
+        FROM likes AS l
+        WHERE l.user_id IN
+              (SELECT user_id
+                  FROM profiles AS p
+                  WHERE p.sex like 'f'))) AS t
+GROUP BY t.SEX ;
+
+
+========================================================================================================================
+
+5. Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной
+сети
+
+я взял лаайки, сообщения, загруженное медиа, и сообщества. По хорошему так конечно никто не делает)))
+но получилось так)))
+
+SELECT
+       CONCAT(first_name,' ',last_name) AS user_name
+       ,if(l.all_likes IS NULL, 0, l.all_likes/TIMESTAMPDIFF(MONTH , created_at , NOW())) AS 'like/month'
+       ,if(m.all_message IS NULL, 0, m.all_message/TIMESTAMPDIFF(MONTH , created_at , NOW()) ) AS 'messages/month'
+       ,if(m2.all_media IS NULL, 0,m2.all_media/TIMESTAMPDIFF(MONTH , created_at , NOW()))  AS 'media/month'
+       ,if(com.all_comm IS NULL, 0, com.all_comm/TIMESTAMPDIFF(MONTH , created_at , NOW())) AS 'comment/month'
+       ,(if(l.all_likes IS NULL, 0, l.all_likes/TIMESTAMPDIFF(MONTH , created_at , NOW()))+
+       if(m.all_message IS NULL, 0, m.all_message/TIMESTAMPDIFF(MONTH , created_at , NOW()))+
+       if(m2.all_media IS NULL, 0,m2.all_media/TIMESTAMPDIFF(MONTH , created_at , NOW()))+
+       if(com.all_comm IS NULL, 0, com.all_comm/TIMESTAMPDIFF(MONTH , created_at , NOW()))) /4 AS AVG_ACT
+from users AS u
+         LEFT JOIN
+     (SELECT user_id, COUNT(target_id)  AS all_likes
+      FROM likes
+      GROUP BY user_id
+     ) AS l
+     on u.id = l.user_id
+         LEFT JOIN
+     (SELECT from_user_id, COUNT(id) AS all_message
+      FROM messages
+      GROUP BY  from_user_id
+     ) AS m
+     on u.id = m.from_user_id
+         LEFT JOIN
+     (SELECT user_id, COUNT(id) all_media
+       FROM media
+       GROUP BY user_id
+      ) AS m2
+       on u.id = m2.user_id
+         LEFT JOIN
+     (SELECT user_id, COUNT(user_id) AS all_comm
+        FROM communities_users
+        GROUP BY user_id) as com
+        on u.id = com.user_id
+
+ORDER BY  AVG_ACT
+              limit 10;
+
+
+
+Как-то все громоздко получается. В челом- все понятно
+
+про partition by кейс пока не придумал)))
