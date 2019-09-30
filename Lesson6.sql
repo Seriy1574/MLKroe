6f512cf486cd2567bb41d1b4c2b348cb1c210f05
Seriy1574 <kroe74@gmail.com>
2019-09-30 12:53:58 +0300

    нормализовал читабельность кода

--- Lesson6.sql
+++ Lesson6.sql
@@ -5,17 +5,20 @@
 1. Проанализировать запросы, которые выполнялись на занятии, определить возможные корректировки и/или улучшения
 (JOIN пока не применять).
 
-По этой задаче все понятно.
+По этой задаче все понятно
 
-SELECT
-  first_name,
-  last_name,
-  (SELECT filename FROM media WHERE id =
-    (SELECT photo_id FROM profiles WHERE user_id = 1)
-  ),
-  (SELECT hometown FROM profiles WHERE user_id = 1)
+SELECT first_name, last_name,
+       (SELECT filename
+          FROM media
+         WHERE id =
+               (SELECT photo_id
+                  FROM profiles
+                 WHERE user_id = 1)),
+       (SELECT hometown
+          FROM profiles
+         WHERE user_id = 1)
   FROM users
-    WHERE id = 1;
+ WHERE id = 1;
 
 /*тут не ограничили тип медиа*/
 
@@ -36,17 +39,17 @@ SELECT
 --Подсчет кто больше писал пользователю
 
 SELECT m.to_user_id, m.from_user_id, count(m.id) all_messages
- FROM messages m
- where m.to_user_id = 648 and m.from_user_id in
+  FROM messages m
+ WHERE m.to_user_id = 648 and m.from_user_id in
        (SELECT friend_id
-         FROM friendship
+          FROM friendship
          WHERE status_id in
                (SELECT id
-                 FROM friendship_statuses
-                  WHERE id = 1) )
-GROUP BY m.to_user_id, m.from_user_id
-ORDER BY all_messages DESC
-LIMIT 1;
+                  FROM friendship_statuses
+                 WHERE id = 1) )
+ GROUP BY m.to_user_id, m.from_user_id
+ ORDER BY all_messages DESC
+ LIMIT 1;
 +------------+--------------+--------------+
 | to_user_id | from_user_id | all_messages |
 +------------+--------------+--------------+
@@ -57,17 +60,21 @@ LIMIT 1;
 
 
 SELECT CONCAT(
-(SELECT CONCAT('Пользователь ',first_name, ' ',last_name, ' получил от пользователя ')
-    from users u where id =
-(SELECT t1.to_user_id from (SELECT m.to_user_id, m.from_user_id, count(m.id) all_messages
-FROM messages m
-where m.to_user_id = 648 and m.from_user_id in
-   (SELECT friend_id
-     FROM friendship
-     WHERE status_id in
-     (SELECT id
-       FROM friendship_statuses
-       WHERE id = 1) )
+       (SELECT CONCAT('Пользователь ',first_name, ' ',last_name, ' получил от пользователя ')
+          FROM users u
+         WHERE id =
+               (SELECT t1.to_user_id
+                  FROM
+                       (SELECT m.to_user_id, m.from_user_id, count(m.id) all_messages
+                          FROM messages m
+                         WHERE m.to_user_id = 648
+                           AND m.from_user_id IN
+                              (SELECT friend_id
+                                 FROM friendship
+                                WHERE status_id IN
+                                      (SELECT id
+                                         FROM friendship_statuses
+                                        WHERE id = 1) )
 GROUP BY m.to_user_id, m.from_user_id
 ORDER BY all_messages DESC
 LIMIT 1) t1)),
@@ -109,18 +116,18 @@ last_name, ' ')
 
 --c джойном красивее получается)
 SELECT CONCAT
-    ('Пользователь ',u1.first_name,' ',u1.last_name,' получил ',t1.all_mess,' сообщ. от пользователя ',u2.first_name,' ',u2.last_name)
-from
-     (SELECT m.to_user_id, m.from_user_id, count(m.id) all_mess
-     FROM messages m
-     where m.to_user_id = 648
-     group by m.to_user_id, m.from_user_id
-     order by all_mess DESC
-     LIMIT 1) t1
-         join users u1
-             on u1.id = t1.to_user_id
-         join users u2
-             on u2.id = t1.from_user_id;
+       ('Пользователь ',u1.first_name,' ',u1.last_name,' получил ',t1.all_mess,' сообщ. от пользователя ',u2.first_name,' ',u2.last_name)
+  FROM
+       (SELECT m.to_user_id, m.from_user_id, count(m.id) all_mess
+          FROM messages m
+         WHERE m.to_user_id = 648
+         GROUP BY m.to_user_id, m.from_user_id
+         ORDER BY all_mess DESC
+         LIMIT 1) t1
+        JOIN users u1
+          ON u1.id = t1.to_user_id
+             JOIN users u2
+             ON u2.id = t1.from_user_id;
 +-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
 | Пользователь Christelle Pfannerstill получил 3 сообщ. от пользователя Winfield Rau                                                                                                  |
 +-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
@@ -134,12 +141,14 @@ from
 /*тут обернул SELECT  в еще один SELECT , чтою сработал LIMIT 10*/
 
 SELECT COUNT(id) AS All_likes
- FROM likes AS l
+  FROM likes AS l
  WHERE l.user_id IN
-  SELECT * FROM (SELECT user_id
-    FROM profiles
-    ORDER BY (TO_DAYS(NOW()) - TO_DAYS(birthday))/365.25
-    limit 10)t2);
+       (SELECT *
+         FROM
+              (SELECT user_id
+                 FROM profiles
+                ORDER BY (TO_DAYS(NOW()) - TO_DAYS(birthday))/365.25
+                LIMIT 10)t2);
 
 +-----------+
 | All_likes |
@@ -153,19 +162,21 @@ SELECT COUNT(id) AS All_likes
 
 
 SELECT t.SEX, COUNT(user_id) AS all_likes
-from(
-    (SELECT l.user_id, 'm' AS SEX
-        FROM likes AS l
-        WHERE l.user_id IN
-             (SELECT user_id
-                 FROM profiles AS p
-                 WHERE p.sex like 'm'))
-     UNION ALL
-    (SELECT l.user_id, 'f' AS SEX
-        FROM likes AS l
-        WHERE l.user_id IN
-              (SELECT user_id
+  FROM(
+       (SELECT l.user_id, 'm' AS SEX
+          FROM likes AS l
+         WHERE l.user_id IN
+               (SELECT user_id
                   FROM profiles AS p
+                 WHERE p.sex like 'm'))
+
+ UNION ALL
+
+        (SELECT l.user_id, 'f' AS SEX
+           FROM likes AS l
+          WHERE l.user_id IN
+                (SELECT user_id
+                   FROM profiles AS p
                   WHERE p.sex like 'f'))) AS t
 GROUP BY t.SEX ;
 
@@ -188,33 +199,30 @@ SELECT
        if(m.all_message IS NULL, 0, m.all_message/TIMESTAMPDIFF(MONTH , created_at , NOW()))+
        if(m2.all_media IS NULL, 0,m2.all_media/TIMESTAMPDIFF(MONTH , created_at , NOW()))+
        if(com.all_comm IS NULL, 0, com.all_comm/TIMESTAMPDIFF(MONTH , created_at , NOW()))) /4 AS AVG_ACT
-from users AS u
-         LEFT JOIN
-     (SELECT user_id, COUNT(target_id)  AS all_likes
-      FROM likes
-      GROUP BY user_id
-     ) AS l
-     on u.id = l.user_id
-         LEFT JOIN
-     (SELECT from_user_id, COUNT(id) AS all_message
-      FROM messages
-      GROUP BY  from_user_id
-     ) AS m
-     on u.id = m.from_user_id
-         LEFT JOIN
-     (SELECT user_id, COUNT(id) all_media
-       FROM media
-       GROUP BY user_id
-      ) AS m2
-       on u.id = m2.user_id
-         LEFT JOIN
-     (SELECT user_id, COUNT(user_id) AS all_comm
-        FROM communities_users
-        GROUP BY user_id) as com
-        on u.id = com.user_id
+FROM users AS u
+     LEFT JOIN
+          (SELECT user_id, COUNT(target_id)  AS all_likes
+             FROM likes
+            GROUP BY user_id) AS l
+     ON u.id = l.user_id
+        LEFT JOIN
+             (SELECT from_user_id, COUNT(id) AS all_message
+                FROM messages
+               GROUP BY  from_user_id) AS m
+        ON u.id = m.from_user_id
+           LEFT JOIN
+                (SELECT user_id, COUNT(id) all_media
+                   FROM media
+                  GROUP BY user_id) AS m2
+           ON u.id = m2.user_id
+              LEFT JOIN
+                   (SELECT user_id, COUNT(user_id) AS all_comm
+                      FROM communities_users
+                     GROUP BY user_id) as com
+              ON u.id = com.user_id
 
 ORDER BY  AVG_ACT
-              limit 10;
+LIMIT 10;
 
 
 
@@ -223,29 +231,6 @@ ORDER BY  AVG_ACT
 про partition by кейс пока не придумал)))
 
 
-
-SELECT COUNT(id) AS All_likes
-FROM likes AS l
-WHERE l.user_id IN
-SELECT * FROM (SELECT user_id
-               FROM profiles
-               ORDER BY (TO_DAYS(NOW()) - TO_DAYS(birthday))/365.25
-               limit 10)t2);
-
-
-SELECT * FROM profiles AS p order by p.birthday DESC LIMIT 10;
-
-SELECT l.user_id, COUNT(id) FROM likes AS l
-WHERE l.user_id  IN (SELECT p.user_id FROM profiles AS p ORDER BY p.birthday DESC LIMIT 10)
-GROUP BY l.user_id;
-
-SELECT p.user_id, p.birthday FROM profiles AS p
-WHERE p.user_id IN(
-    SELECT l.user_id, COUNT(id) FROM likes AS l
-    GROUP BY l.user_id
-    )
-ORDER BY p.birthday DESC
-LIMIT 10;
 
 
 
