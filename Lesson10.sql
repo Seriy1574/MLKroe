90bed1a00ef0e5e6cc837de41c0635465f1c2cc3
Seriy1574 <kroe74@gmail.com>
2019-10-14 17:06:29 +0300

    1 и 3 задание

--- Lesson10.sql
+++ Lesson10.sql
@@ -1,6 +1,6 @@
 ﻿Виктор, приветствую!
 
-Второе и трнетье задание не сделал. нет времени.
+Второе  задание не сделал. нет времени.
 
 1. Проанализировать какие запросы могут выполняться наиболее часто в процессе работы приложения и добавить необходимые индексы.
 
@@ -52,3 +52,70 @@ CREATE INDEX media_ca_ua_idx ON media(created_at, updated_at);
 CREATE INDEX regions_parent_id_idx ON regions(parent_id);
 
 ========================================================================================================================
+Тут я не совсем понял как уложить в один запрос это, в планен визуализации
+
+3. на оконные функции
+Построить запрос, который будет выводить следующие столбцы:
+имя группы
+среднее количество пользователей в группе
+SELECT AVG(av.all_users_in_group)
+FROM
+     (SELECT DISTINCT c.name,
+                      COUNT(cu.user_id) OVER(PARTITION BY c.name) all_users_in_group
+
+      FROM communities c
+           JOIN communities_users cu
+             ON c.id = cu.community_id) av;
+
+без оконной проще запрос быб бы
+
+самый молодой пользователь в группе
+самый пожилой пользователь в группе
+
+SELECT c.name,
+       t2.yang,
+       t3.old
+  FROM communities c
+       LEFT JOIN
+           (SELECT t.name, t.yang
+              FROM
+                   (SELECT DISTINCT c.name,
+                                    CONCAT(u.first_name,' ', u.last_name) yang ,
+                                    ROW_NUMBER() OVER (PARTITION BY c.name ORDER BY p.birthday) RN
+                     FROM communities c
+                          JOIN communities_users cu
+                            ON c.id = cu.community_id
+                               JOIN profiles p
+                                 ON cu.user_id = p.user_id
+                                    JOIN users u
+                                      ON cu.user_id = u.id) t
+            WHERE t.RN = 1) t2
+         ON  c.name = t2.name
+        LEFT JOIN
+                  (SELECT t.name,
+                          t.old
+                     FROM
+                          (SELECT DISTINCT c.name,
+                                           CONCAT(u.first_name,' ', u.last_name) old,
+                                           ROW_NUMBER() OVER (PARTITION BY c.name ORDER BY p.birthday DESC) RN
+                             FROM communities c
+                                  JOIN communities_users cu
+                                    ON c.id = cu.community_id
+                                       JOIN profiles p
+                                         ON cu.user_id = p.user_id
+                                            JOIN users u
+                                              ON cu.user_id = u.id) t
+                   WHERE t.RN = 1) t3
+                      ON c.name = t3.name;
+
+
+общее количество пользователей в группе
+всего пользователей в системе
+отношение в процентах (общее количество пользователей в группе / всего пользователей в системе) * 100
+
+SELECT DISTINCT c.name,
+                COUNT(cu.user_id) OVER(PARTITION BY c.name) all_users_in_group,
+                CONCAT(ROUND(COUNT(cu.user_id) OVER(PARTITION BY c.name)/COUNT(cu.user_id)OVER()*100, 2),'%') share
+
+ FROM communities c
+      JOIN communities_users cu on c.id = cu.community_id;
