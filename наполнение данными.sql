9f5d8c762107fdb7128f2c7f4da48b9fc69edadd
Seriy1574 <kroe74@gmail.com>
2019-10-17 14:38:37 +0300

    наполнение данными

---
+++ наполнение данными.sql
@@ -0,0 +1,30 @@
+﻿USE mobile_collection;
+    INSERT INTO types_actions (id, name)
+    VALUE
+        (DEFAULT, 'Звонок исходящий'),
+        (DEFAULT, 'Звонок взодящий'),
+        (DEFAULT, 'Выезд'),
+        (DEFAULT, 'Отправлено письмо'),
+        (DEFAULT, 'Отправлено СМС');
+
+INSERT INTO types_adresses (id, name)
+    VALUE
+    (DEFAULT, 'Регистрации'),
+    (DEFAULT, 'Проживания'),
+    (DEFAULT, 'Фактический'),
+    (DEFAULT, 'Рабочий'),
+    (DEFAULT, 'Прочие адреса');
+
+INSERT INTO types_phones (id, name)
+    VALUE
+    (DEFAULT, 'Мобильный'),
+    (DEFAULT, 'Рабочий'),
+    (DEFAULT, 'Домашний');
+
+INSERT INTO types_products(id, name)
+    VALUE
+    (DEFAULT, 'Кредитная карта'),
+    (DEFAULT, 'Кредит наличными'),
+    (DEFAULT, 'Ипотека'),
+    (DEFAULT, 'Автокредит'),
+    (DEFAULT, 'Потреб. кредит');
